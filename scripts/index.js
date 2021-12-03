86ca91d5bea520364a36c2ebddf76366e030f57b
Anna Silina <kalemn@yandex.ru>
2021-12-03 15:04:14 +0300

    rewriting js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 // массив карточек мест по умолчанию
-const initialCards = [
+const initialItems = [
 	{
 		name: 'Архыз',
 		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -34,6 +34,22 @@ const userBioElement = profileElement.querySelector('.profile__user-bio');
 // находим галерею мест
 const galleryElement = document.querySelector('.gallery');
 
+// рендерим карточки для галереи мест по умолчанию
+initialItems.forEach(item => {
+	galleryElement.insertAdjacentHTML('beforeend', `
+		<article class="gallery__item">
+			<img class="gallery__photo" src=${item.link} alt=${item.name}>
+			<h2 class="gallery__caption">${item.name}</h2>
+			<button type="button" class="gallery__like-button button"></button>
+			<button type="button" class="gallery__delete-button button"></button>
+		</article>`)
+});
+
+// находим элементы фото и названий мест
+const itemPhotoElement = galleryElement.querySelectorAll('.gallery__photo');
+const itemNameElement = galleryElement.querySelectorAll('.gallery__caption');
+
+
 // находим форму редактирования профиля и поля ввода именя и подписи пользователя
 const profileEditForm = document.querySelector('.popup_type_profile-edit');
 const userNameInput = profileEditForm.querySelector('.popup__form-item_el_user-name');
@@ -42,4 +58,17 @@ const userBioInput = profileEditForm.querySelector('.popup__form-item_el_user-bi
 // находим форму добавления карточки места в галерею
 const itemAddForm = document.querySelector('.popup_type_add-item');
 
-//
+// находим кнопки открытия и закрытия форм
+const openProfileEditForm = profileElement.querySelector('.profile__button-edit');
+const closeProfileEditForm = profileEditForm.querySelector('.popup__close-button');
+const openItemAddForm = profileElement.querySelector('.profile__button-add');
+const closeItemAddForm = itemAddForm.querySelector('.popup__close-button');
+
+// находим кнопки лайков и удаления карточек
+const likeItemButton = galleryElement.querySelectorAll('.gallery__like-button');
+const deleteItemButton = galleryElement.querySelectorAll('.gallery__delete-button');
+
+// функция открытия форм
+function openForm(form) {
+	
+}
